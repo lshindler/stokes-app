fb22af2421cf404618ba34ff4fe3a7812e26a8c3
lshindler <shindler.luca@gmail.com>
2026-04-24 17:08:47 +0200

    Update app.py

--- app.py
+++ app.py
@@ -13,14 +13,14 @@ superficie = st.sidebar.number_input("Superficie deposito [m2]", value=590)
 col1, col2 = st.columns(2)
 
 with col1:
-    st.header("🧪 Modello Gas)")
+    st.header("🧪 Modello Gas")
     v_dep_gas = st.slider("Vdep Gas [m/hr]", 0.001, 0.1, 0.02)
     cout_gas = st.number_input("Conc. Esterna Gas [µg/m3]", value=40.0)
     io_ratio = ach / (v_dep_gas * (superficie/volume) + ach)
     st.metric("Conc. Interna Gas", f"{(cout_gas * io_ratio):.2f} µg/m3")
 
 with col2:
-    st.header("🧹 Modello Polvere)")
+    st.header("🧹 Modello Polvere")
     cin_pm = st.number_input("Conc. Interna PM2.5 [µg/m3]", value=3.3)
     uvl_perc = st.slider("Limite Visivo (UVL) [%]", 0.1, 1.0, 0.2)
     
